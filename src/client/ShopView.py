c3e45d8409698ca3c7bdd8c47aeae11fd3313d7b
naimbenziani <naim599100@gmail.com>
2026-05-12 19:43:31 +0200

    closes #8

--- src/client/ShopView.py
+++ src/client/ShopView.py
@@ -7,7 +7,6 @@ class ShopView(View):
         self.title_label = ctk.CTkLabel(self, text="Boutique", font=ctk.CTkFont(size=20, weight="bold"))
         self.title_label.grid(row=0, column=0, padx=20, pady=20)
 
-
         self.back_button = ctk.CTkButton(self, text="Retour", command=self.back_action)
         self.back_button.grid(row=1, column=0, padx=20, pady=20)
 
