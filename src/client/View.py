68a29f5d4f796d3ad6d9aabd58184c1ebcff5dc8
Naïm Benziani <naim.benziani@ulb.be>
2026-05-15 21:24:31 +0200

    connection of the client Network Manager + chose ip in the GUI

--- src/client/View.py
+++ src/client/View.py
@@ -1,15 +1,17 @@
 from enum import Enum
 from abc import ABC, abstractmethod
 import customtkinter as ctk
+from ClientNetworkManager import ClientNetworkManager
 
 class TypeView(Enum):
     LOGIN = 0
     APP = 1
 
 class View(ctk.CTkFrame, ABC):
-    def __init__(self, parent, controller):
+    def __init__(self, parent, controller, manager: ClientNetworkManager ):
         super().__init__(parent)
         self.controller = controller
+        self.manager = manager
         self.initView()
 
     @abstractmethod
