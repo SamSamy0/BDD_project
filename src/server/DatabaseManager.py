2f952013b79ea572b6ec0868a7e42ddae6efaa85
SamSamy0 <scontuliano@gmail.com>
2026-05-17 12:45:33 +0200

    DBManager functions to interact with server

--- src/server/DatabaseManager.py
+++ src/server/DatabaseManager.py
@@ -7,7 +7,27 @@ class DatabaseManager:
         self.conn = cursor
         self.path_signin = "DB/queries/users/signin.sql"
         self.path_signup = "DB/queries/users/signup.sql"
+        # Courses
         self.path_getAllCourses = "DB/queries/courses/list_courses.sql"
+        self.path_addCourse = "DB/queries/courses/add_course.sql"
+        self.path_deleteUserCourse = "DB/queries/users/delete_user_course.sql"
+        self.path_getUserCourse = "DB/queries/courses/list_user_cours.sql"
+        # Review
+        self.path_addReview = "DB/queries/review/add_evaluation.sql"
+        # Shop
+        self.path_buyObjet = "DB/queries/shop/add_object_to_users.sql"
+        self.path_getPoints = "DB/queries/shop/check_points.sql"
+        self.path_getTransactionHistory = (
+            "DB/queries/shop/check_transaction_history.sql"
+        )
+        self.path_getObjectInfo = "DB/queries/shop/inspect_object.sql"
+        # Summaries
+        self.path_addSummary = "DB/queries/summaries/add_summary.sql"
+        self.path_checkSummary = "DB/queries/summaries/check_summary.sql"
+        self.path_deleteSummary = "DB/queries/summaries/delete_summary.sql"
+        self.path_getSummAverage = "DB/queries/stats/summary_average.sql"
+        # User
+        self.path_changeStateObj = "DB/queries/users/changeStataeObj"
 
     def reader_query(self, path, fetch="all", insert=False, params=None):
         with open(path, "r", encoding="utf-8") as fichier:
@@ -49,3 +69,49 @@ class DatabaseManager:
 
     def getAllCourses(self, data):
         return self.reader_query(self.path_getAllCourses, "all", False, params=data)
+
+    # Courses
+    def addCourse(self, data):
+        return self.reader_query(self.path_addCourse, "one", True, params=data)
+
+    def deleteUserCourse(self, data):
+        return self.reader_query(self.path_deleteUserCourse, "one", True, params=data)
+
+    def getUserCourse(self, data):
+        return self.reader_query(self.path_getUserCourse, "all", False, params=data)
+
+    # Review
+    def addReview(self, data):
+        return self.reader_query(self.path_addReview, "one", True, params=data)
+
+    # Shop
+    def buyObject(self, data):
+        return self.reader_query(self.path_buyObjet, "one", True, params=data)
+
+    def getPoints(self, data):
+        return self.reader_query(self.path_getPoints, "one", False, params=data)
+
+    def getTransactionHistory(self, data):
+        return self.reader_query(
+            self.path_getTransactionHistory, "all", False, params=data
+        )
+
+    def getObjectInfo(self, data):
+        return self.reader_query(self.path_getObjectInfo, "all", False, params=data)
+
+    # Summaries
+    def addSummary(self, data):
+        return self.reader_query(self.path_addSummary, "one", True, params=data)
+
+    def checkSummary(self, data):
+        return self.reader_query(self.path_checkSummary, "all", False, params=data)
+
+    def deleteSummary(self, data):
+        return self.reader_query(self.path_deleteSummary, "one", True, params=data)
+
+    def getSummAverage(self, data):
+        return self.reader_query(self.path_getSummAverage, "one", False, params=data)
+
+    # User
+    def changeStateObj(self, data):
+        return self.reader_query(self.path_changeStateObj, "one", False, params=data)
