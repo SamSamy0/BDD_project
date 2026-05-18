edfcb6cbc0bfe604aa92240f8d6fdb99b01d29cb
SamSamy0 <130299003+SamSamy0@users.noreply.github.com>
2026-05-18 10:31:28 +0200

    Fix SQL syntax in modify_summary.sql

--- src/DB/queries/summaries/modify_summury.sql
+++ src/DB/queries/summaries/modify_summury.sql
@@ -1,4 +1,4 @@
-²SELECT r.ID r.Titre,r.Description, r.Version, r.Mnemonique
+SELECT r.ID r.Titre,r.Description, r.Version, r.Mnemonique
 FROM Resume r
 WHERE r.ID = %(idSumm)s and  r.Titre = %(title)s and  r.Description = %s
     and  r.Version = %(version)s and  r.Mnemonique = %(mnemo)s
