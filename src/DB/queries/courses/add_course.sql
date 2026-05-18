61008eec68702faf5103eff77cae8c82c175f72f
SamSamy0 <130299003+SamSamy0@users.noreply.github.com>
2026-05-18 10:37:08 +0200

    Update add_course.sql

--- src/DB/queries/courses/add_course.sql
+++ src/DB/queries/courses/add_course.sql
@@ -1,2 +1,2 @@
 -- Add a course
-INSERT INTO Cours (Mnemonique, Nom, Fac, Credits, Annee) VALUES (%(mnemo)s, %(name)s, %(fact)s, %(utc)s, %(year)s )
+INSERT INTO Cours (Mnemonique, Nom, Fac, Credits, Annee) VALUES (%(mnemo)s, %(name)s, %(fac)s, %(utc)s, %(year)s )
