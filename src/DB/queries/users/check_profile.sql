7c332370188c47e9bfd11bdf4966e699813d11f0
SamSamy0 <scontuliano@gmail.com>
2026-05-19 11:20:32 +0200

    Title is Updated when User active or deactivate it in the shop

--- src/DB/queries/users/check_profile.sql
+++ src/DB/queries/users/check_profile.sql
@@ -1,5 +1,9 @@
-SELECT u.Nom, u.Niveau, u.Points, uo.IdObjet AS IdObjet, oc.Nom AS NomObjet
+SELECT u.Nom, u.Niveau, u.Points, Titre.IdObjet, Titre.NomObjet AS NomObjet
 FROM Utilisateur u 
-LEFT JOIN UtilisateurObjet uo ON u.ID = uo.IdUtilisateur JOIN ObjetCosmetique oc ON oc.ID = uo.IdObjet
-AND uo.EstActif = 1 AND oc.TypeObjet = 'Titre'
+LEFT JOIN (
+  SELECT uo.IdUtilisateur, uo.IdObjet, oc.nom AS NomObjet
+  FROM UtilisateurObjet uo
+  JOIN ObjetCosmetique oc on uo.IdObjet = oc.ID
+  WHERE EstActif = 1 AND oc.TypeObjet = 'Titre'
+) AS Titre ON u.ID = Titre.IdUtilisateur
 WHERE u.ID = 5;
--- src/client/ProfilView.py
+++ src/client/ProfilView.py
@@ -12,6 +12,18 @@ class ProfilView(View):
         )
         self.title_label.grid(row=0, column=0, padx=20, pady=20)
 
+        self.name_label = ctk.CTkLabel(self, text="")
+        self.name_label.grid(row=1, column=0, padx=20, pady=10)
+
+        self.points_label = ctk.CTkLabel(self, text="")
+        self.points_label.grid(row=2, column=0, padx=20, pady=10)
+
+        self.level_label = ctk.CTkLabel(self, text="")
+        self.level_label.grid(row=3, column=0, padx=20, pady=10)
+
+        self.title_active_label = ctk.CTkLabel(self, text="")
+        self.title_active_label.grid(row=4, column=0, padx=20, pady=10)
+
         self.back_button = ctk.CTkButton(self, text="Retour", command=self.back_action)
         self.back_button.grid(row=5, column=0, padx=20, pady=20)
 
@@ -19,25 +31,14 @@ class ProfilView(View):
         self.controller.show_view("MENU")
 
     def displayStats(self, data):
-        name = data.get("Nom")
-        level = data.get("Niveau")
-        points = data.get("Points")
-        title = data.get("NomObjet")
+        name = data.get("Nom", "Sans Nom")
+        level = data.get("Niveau", "Sans Niveau")
+        points = data.get("Points", "Pas de Points")
+        title = data.get("NomObjet", "Aucun Objet équipé")
 
-        # nom de l'utilisateur
-        self.name_label = ctk.CTkLabel(self, text=f"Nom : {name}")
-        self.name_label.grid(row=1, column=0, padx=20, pady=10)
-        # points
-        self.points_label = ctk.CTkLabel(self, text=f"Points : {points}")
-        self.points_label.grid(row=2, column=0, padx=20, pady=10)
-        # niveau
-        self.level_label = ctk.CTkLabel(self, text=f"Niveau : {level}")
-        self.level_label.grid(row=3, column=0, padx=20, pady=10)
-        # titre actif
-        if title:
-            title_text = f"Titre : {title}"
-        else:
-            title_text = "Aucun objet équipé"
-        self.title_active_label = ctk.CTkLabel(self, text=title_text)
+        title_text = f"Titre : {title}" if title else "Aucun objet équipé"
 
-        self.title_active_label.grid(row=4, column=0, padx=20, pady=10)
+        self.name_label.configure(text=f"Nom : {name}")
+        self.points_label.configure(text=f"Points : {points}")
+        self.level_label.configure(text=f"Niveau : {level}")
+        self.title_active_label.configure(text=title_text)
