17c8fbfe2168edfb463f9cc49b736e020f167ca8
Dan2569 <musampadaniel@gmail.com>
2026-05-20 08:54:59 +0200

    Fix the execute interface (with IA)

--- src/client/Gui.py
+++ src/client/Gui.py
@@ -17,6 +17,8 @@ class Gui(ctk.CTk):
         super().__init__()
         self.manager = manager
 
+        self.withdraw() #permet cacher les fenêtre quand on execute le client
+
         self.title("BDD")
         self.geometry("1000x1000")
         self.middlex = self.winfo_screenwidth() / 2
@@ -47,6 +49,8 @@ class Gui(ctk.CTk):
 
         # Gérer la fermeture de la fenêtre
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+        self.deiconify() # on réaffiche  la fenêtre proprement après l'installation des fenêtres
 
     def on_closing(self, event=None):
         """Action effectuée à la fermeture de la fenêtre."""
