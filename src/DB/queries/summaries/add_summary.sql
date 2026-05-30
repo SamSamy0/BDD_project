acdaa5e00868399dc7042ce19f2612503aa721bb
Dan2569 <126469588+Dan2569@users.noreply.github.com>
2026-05-30 13:45:17 +0200

    Fix SQL insert statement for Resume table

--- src/DB/queries/summaries/add_summary.sql
+++ src/DB/queries/summaries/add_summary.sql
@@ -1,6 +1,2 @@
--- Add a summerize
-<<<<<<< HEAD
-INSERT INTO Resume (Titre, Description, Publication, Version, Visibilite, Moyenne, Mnemonique, IdUtilisateur) VALUES (%(title)s, %(desc)s, %(date)s, %(version)s,%(visible)s, NULL, %(mnemo)s, %(idAuthor)s );
-=======
 INSERT INTO Resume (Titre, Description, Publication, Version, Visibilite, Moyenne, Mnemonique, IdUtilisateur) VALUES (%(title)s, %(desc)s, %(date)s, 1 ,%(visible)s, NULL, %(mnemo)s, %(idAuthor)s )
->>>>>>> main
+
