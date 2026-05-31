ab8c2971bc037934d992813d8afa5098e8ffc687
naimbenziani <naim599100@gmail.com>
2026-05-31 22:01:42 +0200

    fix add_summary.sql
    version always 1

--- src/DB/queries/summaries/add_summary.sql
+++ src/DB/queries/summaries/add_summary.sql
@@ -1,5 +1,5 @@
 INSERT INTO Resume (Titre, Description, Publication, Version, Visibilite, Moyenne, Mnemonique, IdUtilisateur)
-SELECT %(title)s, %(desc)s, %(date)s, %(version)s, %(visible)s, NULL, %(mnemo)s, %(idAuthor)s
+SELECT %(title)s, %(desc)s, %(date)s, 1, %(visible)s, NULL, %(mnemo)s, %(idAuthor)s
 FROM CoursUtilisateur
 WHERE Mnemonique = %(mnemo)s AND IdUtilisateur = %(idAuthor)s;
 
